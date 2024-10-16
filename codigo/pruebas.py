d9a1513b561219d3a959bfbd5c5d52d38cac881b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-16 08:03:00 +0200

    init

---
+++ codigo/pruebas.py
@@ -0,0 +1,13 @@
+"""
+Ejemplos en python:
+-1) Crear una lista y operaciones básicas
+"""
+
+from random import randint
+
+def suma(a:str,b:str) -> str:
+    return a+b
+
+print(suma(12,34))
+print(suma("12","34"))
+print(suma([1,2],[3,4]))
